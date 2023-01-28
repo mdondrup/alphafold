d126c14942338114cc240e572583af713c3feed5
Michael Dondrup <mdondrup@users.noreply.github.com>
2023-01-28 15:02:39 +0100

    Create test_imports.py

---
+++ test_imports.py
@@ -0,0 +1,25 @@
+import json
+import os
+import pathlib
+import pickle
+import random
+import shutil
+import sys
+import time
+from typing import Dict, Union
+
+from absl import app
+from absl import flags
+from absl import logging
+from alphafold.common import protein
+from alphafold.common import residue_constants
+from alphafold.data import pipeline
+from alphafold.data import pipeline_multimer
+from alphafold.data import templates
+from alphafold.data.tools import hhsearch
+from alphafold.data.tools import hmmsearch
+from alphafold.model import config
+from alphafold.model import data
+from alphafold.model import model
+from alphafold.relax import relax
+import numpy as np
